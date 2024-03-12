8d14a1e43d38d9763bbab0fbe73e9b357915bbed
Asuman yağmur Aybek <161846356+salomeanta@users.noreply.github.com>
2024-03-12 13:59:39 +0300

    Update 8week1.sql

--- 8week1.sql
+++ 8week1.sql
@@ -1,3 +1,28 @@
+# 🍜 Case Study #1: Danny's Diner 
+<img src="https://user-images.githubusercontent.com/81607668/127727503-9d9e7a25-93cb-4f95-8bd0-20b87cb4b459.png" alt="Image" width="500" height="520">
+
+## 📚 Table of Contents
+- [Business Task](#business-task)
+- [Entity Relationship Diagram](#entity-relationship-diagram)
+- [Question and Solution](#question-and-solution)
+
+Please note that all the information regarding the case study has been sourced from the following link: [here](https://8weeksqlchallenge.com/case-study-1/). 
+
+***
+
+## Business Task
+Danny wants to use the data to answer a few simple questions about his customers, especially about their visiting patterns, how much money they’ve spent and also which menu items are their favourite. 
+
+***
+
+## Entity Relationship Diagram
+
+![image](https://user-images.githubusercontent.com/81607668/127271130-dca9aedd-4ca9-4ed8-b6ec-1e1920dca4a8.png)
+
+***
+
+
+
 --1)Her müşterinin restoranda harcadığı toplam tutar nedir?
 --What is the total amount each customer spent at the restaurant?
 select customer_id,
